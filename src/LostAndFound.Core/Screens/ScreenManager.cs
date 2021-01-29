ba4bdca38982da5c81ed282cdef5bffcc23319e6
maemdk <maemdk@gmail.com>
2021-01-29 22:52:26 +0100

    Create implementation ScreenManager.cs

---
+++ src/LostAndFound.Core/Screens/ScreenManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LostAndFound.Core.Screens
+{
+    class ScreenManager : IScreenManager
+    {
+        private readonly IReadOnlyCollection<IScreen> _screens;
+
+        public ScreenManager(IReadOnlyCollection<IScreen> screens)
+        {
+            _screens = screens;
+        }
+        
+        public void SetActiveScreen<T>() where T : IScreen
+        {
+            
+        }
+
+        public IScreen GetActiveScreen()
+        {
+            return null;
+        }
+    }
+}
