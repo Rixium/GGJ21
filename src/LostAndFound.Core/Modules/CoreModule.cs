75ed2133acc5ff1f77c8d9d941cbcf96e5ce43b1
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-29 23:17:46 +0000

    Add aseprite content deserializer

--- src/LostAndFound.Core/Modules/CoreModule.cs
+++ src/LostAndFound.Core/Modules/CoreModule.cs
@@ -3,6 +3,7 @@ using LostAndFound.Core.Config;
 using LostAndFound.Core.Content;
 using LostAndFound.Core.Graphics;
 using LostAndFound.Core.Screens;
+using LostAndFound.Core.System;
 using Microsoft.Xna.Framework;
 
 namespace LostAndFound.Core.Modules
@@ -13,14 +14,17 @@ namespace LostAndFound.Core.Modules
         {
             builder.RegisterType<Game1>().As<Game>();
 
+            builder.RegisterType<FileSystem>().As<IFileSystem>().InstancePerDependency();
+            builder.RegisterType<ContentDeserializer>().As<IContentDeserializer>().InstancePerDependency();
+
             builder.RegisterType<RenderManager>().As<IRenderManager>().SingleInstance();
-            
+
             builder.RegisterType<ScreenManager>().As<IScreenManager>().SingleInstance();
 
             builder.RegisterType<SplashScreen>().As<IScreen>();
             builder.RegisterType<EmptyScreen>().As<IScreen>();
             builder.RegisterType<MainMenuScreen>().As<IScreen>();
-            
+
             builder.RegisterType<ContentChest>().As<IContentChest>().SingleInstance();
 
             builder.RegisterType<WindowConfiguration>().As<IWindowConfiguration>().SingleInstance();
