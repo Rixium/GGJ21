97b1751196d13fa5792d9f0b173a02df0a2ccc9f
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-29 17:45:06 +0000

    use autofac for dependency injection

---
+++ src/LostAndFound.Core/Modules/CoreModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Microsoft.Xna.Framework;
+
+namespace LostAndFound.Core.Modules
+{
+    public class CoreModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Game1>().As<Game>();
+            base.Load(builder);
+        }
+    }
+}
--- src/LostAndFound.Core/Program.cs
+++ src/LostAndFound.Core/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using Autofac;
+using LostAndFound.Core.Modules;
+using Microsoft.Xna.Framework;
 
 namespace LostAndFound.Core
 {
     public static class Program
     {
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            using (var game = new Game1())
-                game.Run();
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<CoreModule>();
+            var container = containerBuilder.Build();
+
+            using var game = container.Resolve<Game>();
+            game.Run();
         }
     }
 }
