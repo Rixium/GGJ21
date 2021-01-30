421eb8a7c6004c067cfe1a501abf87e6bdb700ff
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-30 14:04:37 +0000

    add camera to game instance

---
+++ src/LostAndFound.Core/Games/Camera.cs
@@ -0,0 +1,67 @@
+﻿using LostAndFound.Core.Config;
+using Microsoft.Xna.Framework;
+
+namespace LostAndFound.Core.Games
+{
+    public class Camera
+    {
+        private readonly IWindowConfiguration _windowConfiguration;
+        private Vector2 _position;
+
+        private int _minX;
+        private readonly int _maxX;
+        private int _minY;
+        private readonly int _maxY;
+
+        private int _zoom = 1;
+
+        public Camera(IWindowConfiguration windowConfiguration)
+        {
+            _windowConfiguration = windowConfiguration;
+            _position = Vector2.Zero;
+            _maxX = 2000000;
+            _maxY = 1000000;
+        }
+        
+        public Vector2 ToGo { get; set; }
+
+        public void Update(int maxX, int maxY)
+        {
+            maxX -= _windowConfiguration.WindowWidth / 2 / _zoom + 3;
+            maxY -= _windowConfiguration.WindowHeight / 2 / _zoom + 3;
+            _minX = 0;
+            _minY = 0;
+
+            var (x, y) = Vector2.Lerp(Position, ToGo, 0.001f);
+            
+            if (Vector2.Distance(Position, ToGo) < 10)
+            {
+                return;
+            }
+
+            _position.X = x;
+            _position.Y = y;
+
+            _position.X = MathHelper.Clamp(_position.X, _minX, maxX);
+            _position.Y = MathHelper.Clamp(_position.Y, _minY, maxY);
+        }
+
+        public Vector2 Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+
+        public Matrix GetMatrix() =>
+            Matrix.CreateTranslation(new Vector3(-_position.X, -_position.Y, 0)) *
+            Matrix.CreateScale(_zoom, _zoom, 1) *
+            Matrix.CreateTranslation(new Vector3(_windowConfiguration.Center.X, _windowConfiguration.Center.Y, 0));
+
+        public bool Intersects(Rectangle bounds) =>
+            Bounds.Intersects(bounds);
+
+        public Rectangle Bounds => new Rectangle((int) (_position.X - _windowConfiguration.Center.X),
+            (int) (_position.Y - _windowConfiguration.Center.Y), _windowConfiguration.WindowWidth,
+            _windowConfiguration.WindowHeight);
+    }
+}
--- src/LostAndFound.Core/Games/GameInstance.cs
+++ src/LostAndFound.Core/Games/GameInstance.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using LostAndFound.Core.Config;
 using LostAndFound.Core.Games.Models;
 using LostAndFound.Core.Games.Zones;
 using LostAndFound.Core.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace LostAndFound.Core.Games
 {
@@ -11,35 +14,44 @@ namespace LostAndFound.Core.Games
     {
         private readonly IRenderManager _renderManager;
         private readonly IZoneLoader _zoneLoader;
-        
+        private readonly Camera _camera;
+
         private GameData _gameData = new GameData();
         private IList<ZoneData> _zoneData;
 
-        public GameInstance(IRenderManager renderManager, IZoneLoader zoneLoader)
+        public GameInstance(IRenderManager renderManager, IZoneLoader zoneLoader,
+            IWindowConfiguration windowConfiguration)
         {
             _renderManager = renderManager;
             _zoneLoader = zoneLoader;
+            _camera = new Camera(windowConfiguration);
         }
 
         public void Load()
         {
             _zoneData = _zoneLoader.LoadZones();
-            
+
             _gameData.ActiveZone = ZoneType.Test;
+            _camera.ToGo = new Vector2(500, 500);
         }
 
         public void Draw()
         {
-            _renderManager.SpriteBatch.Begin();
+            _renderManager.SpriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null,
+                _camera.GetMatrix());
 
             var activeZone = _zoneData.First(x => x.ZoneType == _gameData.ActiveZone);
             _renderManager.SpriteBatch.Draw(activeZone.BackgroundImage, new Vector2(0, 0), Color.White);
-            
+
             _renderManager.SpriteBatch.End();
         }
 
         public void Update(GameTime gameTime)
         {
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                _camera.Update(20000, 20000);
+            }
         }
     }
 }
