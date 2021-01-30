16da007e13049355052a346763b23193c4ae41c8
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-30 11:08:02 +0000

    draw active zone

--- src/LostAndFound.Core/Games/GameInstance.cs
+++ src/LostAndFound.Core/Games/GameInstance.cs
@@ -1,33 +1,43 @@
-﻿using LostAndFound.Core.Games.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using LostAndFound.Core.Games.Models;
 using LostAndFound.Core.Games.Zones;
+using LostAndFound.Core.Graphics;
 using Microsoft.Xna.Framework;
 
 namespace LostAndFound.Core.Games
 {
     class GameInstance : IGameInstance
     {
+        private readonly IRenderManager _renderManager;
         private readonly IZoneLoader _zoneLoader;
+        
         private GameData _gameData = new GameData();
-        private ZoneData _zoneData;
+        private IList<ZoneData> _zoneData;
 
-        public GameInstance(IZoneLoader zoneLoader)
+        public GameInstance(IRenderManager renderManager, IZoneLoader zoneLoader)
         {
+            _renderManager = renderManager;
             _zoneLoader = zoneLoader;
         }
 
         public void Load()
         {
-            _zoneLoader.LoadZones();
+            _zoneData = _zoneLoader.LoadZones();
         }
-        
+
         public void Draw()
         {
+            _renderManager.SpriteBatch.Begin();
+
+            var activeZone = _zoneData.First(x => x.ZoneType == _gameData.ActiveZone);
+            _renderManager.SpriteBatch.Draw(activeZone.BackgroundImage, new Vector2(0, 0), Color.White);
             
+            _renderManager.SpriteBatch.End();
         }
 
         public void Update(GameTime gameTime)
         {
-            
         }
     }
 }
