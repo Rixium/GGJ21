898f5e058fd7512b91f9a07a406e5941b376b510
maemdk <maemdk@gmail.com>
2021-01-30 20:54:21 +0100

    Extracted ZoneInteractionComponent.cs

--- src/LostAndFound.Core/Games/Components/PlayerControllerComponent.cs
+++ src/LostAndFound.Core/Games/Components/PlayerControllerComponent.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using LostAndFound.Core.Games.Models;
-using LostAndFound.Core.Games.Zones;
-using LostAndFound.Core.Input;
+﻿using LostAndFound.Core.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -12,59 +9,6 @@ namespace LostAndFound.Core.Games.Components
     {
         Left,
         Right
-    }
-
-    public class ZoneInteractionComponent : IComponent
-    {
-        private readonly IGameInstance _gameInstance;
-        public IEntity Entity { get; set; }
-
-        public ZoneInteractionComponent(IGameInstance gameInstance)
-        {
-            _gameInstance = gameInstance;
-        }
-
-        public void Start()
-        {
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            var entityZone = _gameInstance.ActiveZone;
-            foreach (var collider in entityZone.Colliders)
-            {
-                if (!collider.Bounds.Contains(Entity.Position))
-                {
-                    continue;
-                }
-
-                var toProperty = collider.GetProperty("To");
-                if (toProperty == null) continue;
-
-                var toZoneValues = toProperty.Split(',');
-                Enum.TryParse<ZoneType>(toZoneValues[0], out var zoneType);
-                Enum.TryParse<Direction>(toZoneValues[1], out var entranceDirection);
-
-                var zoneToGoTo = _gameInstance.GetZone(zoneType);
-
-                if (zoneToGoTo != null)
-                {
-                    TeleportToZone(entityZone, zoneToGoTo, entranceDirection);
-                }
-            }
-        }
-
-        private void TeleportToZone(IZone oldZone, IZone zoneToGoTo, Direction direction)
-        {
-            _gameInstance.MoveEntityToZone(oldZone, zoneToGoTo, Entity);
-            _gameInstance.SetActiveZone(zoneToGoTo.ZoneType);
-
-            Entity.Position = zoneToGoTo.GetTeleportPoint(direction);
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-        }
     }
 
     public class PlayerControllerComponent : IComponent
---
+++ src/LostAndFound.Core/Games/Components/ZoneInteractionComponent.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using LostAndFound.Core.Games.Models;
+using LostAndFound.Core.Games.Zones;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LostAndFound.Core.Games.Components
+{
+    public class ZoneInteractionComponent : IComponent
+    {
+        private readonly IGameInstance _gameInstance;
+        public IEntity Entity { get; set; }
+
+        public ZoneInteractionComponent(IGameInstance gameInstance)
+        {
+            _gameInstance = gameInstance;
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var entityZone = _gameInstance.ActiveZone;
+            foreach (var collider in entityZone.Colliders)
+            {
+                if (!collider.Bounds.Contains(Entity.Position))
+                {
+                    continue;
+                }
+
+                var toProperty = collider.GetProperty("To");
+                if (toProperty == null) continue;
+
+                var toZoneValues = toProperty.Split(',');
+                Enum.TryParse<ZoneType>(toZoneValues[0], out var zoneType);
+                Enum.TryParse<Direction>(toZoneValues[1], out var entranceDirection);
+
+                var zoneToGoTo = _gameInstance.GetZone(zoneType);
+
+                if (zoneToGoTo != null)
+                {
+                    TeleportToZone(entityZone, zoneToGoTo, entranceDirection);
+                }
+            }
+        }
+
+        private void TeleportToZone(IZone oldZone, IZone zoneToGoTo, Direction direction)
+        {
+            _gameInstance.MoveEntityToZone(oldZone, zoneToGoTo, Entity);
+            _gameInstance.SetActiveZone(zoneToGoTo.ZoneType);
+
+            Entity.Position = zoneToGoTo.GetTeleportPoint(direction);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
