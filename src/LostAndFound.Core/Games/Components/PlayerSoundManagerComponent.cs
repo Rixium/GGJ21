be5517072a67395969afcd48d6ca2c9b790ff9c8
maemdk <maemdk@gmail.com>
2021-01-31 00:57:04 +0100

    Added random footsteps

--- src/LostAndFound.Core/Games/Components/PlayerSoundManagerComponent.cs
+++ src/LostAndFound.Core/Games/Components/PlayerSoundManagerComponent.cs
@@ -15,17 +15,27 @@ namespace LostAndFound.Core.Games.Components
         private PlayerControllerComponent _playerControllerComponent;
         
         private Vector2 lastFootstepPos;
-        private int footStepLenght = 17;
+        private int footStepLenght = 25;
+        List<string> soundPaths = new List<string>();
         
         public void Start()
         {
+            _soundComponent = Entity.GetComponent<SoundComponent>();
+            _playerControllerComponent = Entity.GetComponent<PlayerControllerComponent>();
+            lastFootstepPos = Entity.Position;
+            
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_1");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_2");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_3");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_4");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_5");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_6");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_7");
+            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_8");
         }
 
         public void Update(GameTime gameTime)
         {
-            _soundComponent = Entity.GetComponent<SoundComponent>();
-            _playerControllerComponent = Entity.GetComponent<PlayerControllerComponent>();
-
             if ((lastFootstepPos - Entity.Position).Length() > footStepLenght)
             {
                 lastFootstepPos = Entity.Position;
@@ -35,17 +45,9 @@ namespace LostAndFound.Core.Games.Components
 
         private void PlayRandomFootstep()
         {
-            List<string> soundPaths = new List<string>();
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_1");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_2");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_3");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_4");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_5");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_6");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_7");
-            soundPaths.Add("Audio/SoundEffects/Footsteps/footstep_8");
-            
-            _soundComponent.PlaySoundByName("Audio/SoundEffects/Footstep");
+            Random random = new Random();
+            var i = random.Next(0, 7);
+            _soundComponent.PlaySoundByName(soundPaths[i]);
         }
 
         public void Draw(SpriteBatch spriteBatch)
