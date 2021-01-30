cbbe5722e34285fc59770925fb1ba7159d02ad62
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-30 00:08:19 +0000

    Add gametime extensions

---
+++ src/LostAndFound.Core/Extensions/GameTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LostAndFound.Core.Extensions
+{
+    public static class GameTimeExtensions
+    {
+        public static float AsDelta(this GameTime gameTime) =>
+            (float) gameTime.ElapsedGameTime.TotalMilliseconds / 1000.0f;
+    }
+}
