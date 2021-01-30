629d1998bd7e1eb35b7625858da6b0bac5923a37
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-30 11:25:54 +0000

    use spritemap loader in main menu screen

--- src/LostAndFound.Core/Screens/MainMenuScreen.cs
+++ src/LostAndFound.Core/Screens/MainMenuScreen.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using LostAndFound.Core.Content.Aseprite;
+using LostAndFound.Core.Content.ContentLoader;
 using LostAndFound.Core.Graphics;
 using LostAndFound.Core.Transitions;
+using LostAndFound.Core.UI;
 using Microsoft.Xna.Framework;
 
 namespace LostAndFound.Core.Screens
@@ -9,18 +13,23 @@ namespace LostAndFound.Core.Screens
     {
         private readonly IRenderManager _renderManager;
         private readonly ITransitionManager _transitionManager;
+        private readonly IContentLoader<AsepriteSpriteMap> _spriteMapLoader;
         public Action<ScreenType> RequestScreenChange { get; set; }
         public ScreenType ScreenType => ScreenType.MainMenu;
 
-        public MainMenuScreen(IRenderManager renderManager, ITransitionManager transitionManager)
+        private IList<IPanel> _mainMenuPanels = new List<IPanel>();
+
+        public MainMenuScreen(IRenderManager renderManager, ITransitionManager transitionManager, IContentLoader<AsepriteSpriteMap> spriteMapLoader)
         {
             _renderManager = renderManager;
             _transitionManager = transitionManager;
+            _spriteMapLoader = spriteMapLoader;
         }
-        
+
         public void Load()
         {
             _transitionManager.Load();
+            _spriteMapLoader.GetContent("");
         }
 
         public void Update(GameTime gameTime)
