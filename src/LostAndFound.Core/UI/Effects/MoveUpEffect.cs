0410c8f15f6d20e7b4a5e2c2e35aefd429da4c3e
Daniel Pottenger <gamerpotty@gmail.com>
2021-01-31 11:51:40 +0000

    add move up effect and update park

---
+++ src/LostAndFound.Core/UI/Effects/MoveUpEffect.cs
@@ -0,0 +1,12 @@
+﻿using LostAndFound.Core.Extensions;
+using Microsoft.Xna.Framework;
+
+namespace LostAndFound.Core.UI.Effects
+{
+    public class MoveUpEffect : IElementEffect
+    {
+        public IElement Element { get; set; }
+
+        public void Update(GameTime gameTime) => Element.Position -= new Vector2(0, gameTime.AsDelta() * 10);
+    }
+}
