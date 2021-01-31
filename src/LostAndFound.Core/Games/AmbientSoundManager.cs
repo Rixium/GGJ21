3530b86bc7803eb572a909b9cab861b6f0469641
maemdk <maemdk@gmail.com>
2021-01-31 02:06:28 +0100

    Added some ambient sound to street

--- src/LostAndFound.Core/Games/AmbientSoundManager.cs
+++ src/LostAndFound.Core/Games/AmbientSoundManager.cs
@@ -1,28 +1,53 @@
-﻿using LostAndFound.Core.Games.Models;
+﻿using System;
+using LostAndFound.Core.Content;
+using LostAndFound.Core.Games.Models;
 using LostAndFound.Core.Games.Systems;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
-using NotImplementedException = System.NotImplementedException;
 
 namespace LostAndFound.Core.Games
 {
     public class AmbientSoundManager : ISystem
     {
         private readonly ZoneManager _zoneManager;
+        private readonly IContentChest _contentChest;
 
-        public AmbientSoundManager(ZoneManager zoneManager)
+        private SoundEffectInstance _ambientInstance;
+        private SoundEffect _streetAmbientSound;
+
+        public AmbientSoundManager(ZoneManager zoneManager, IContentChest contentChest)
         {
             _zoneManager = zoneManager;
+            _contentChest = contentChest;
             zoneManager.ZoneChanged += OnZoneChanged;
         }
 
         void OnZoneChanged(ZoneType zoneType)
         {
-            
+            _ambientInstance.Stop();
+            switch (zoneType)
+            {
+                case ZoneType.None:
+                    break;
+                case ZoneType.Test:
+                    break;
+                case ZoneType.Street:
+                    _ambientInstance = _streetAmbientSound.CreateInstance();
+                    break;
+                case ZoneType.Forest:
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(zoneType), zoneType, null);
+            }
+            _ambientInstance.Play();
         }
 
         public void Start()
         {
+            _streetAmbientSound = _contentChest.Get<SoundEffect>("Audio/Ambience/StreetAmbience");
+            _ambientInstance = _streetAmbientSound.CreateInstance();
+            _ambientInstance.Play();
         }
 
         public void Update(GameTime gameTime)
